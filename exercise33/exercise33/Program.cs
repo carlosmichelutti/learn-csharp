9c339de9333435909aa486465f5a1e9cbe09dbb0
Carlos Michelutti <carlos.stz590@gmail.com>
2025-12-01 22:09:51 -0300

    Create exercise33.

---
+++ exercise33/exercise33/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace exercise33;
+
+public class Program
+{
+    public static void Main()
+    {
+        string[] namesArray = { "Fernanda", "Eduardo", "Gustavo", "Carolina", "Alice", "Bruno", "Daniel" };
+        namesArray.Sort();
+        Console.WriteLine($"The third name in the list after sorting is: {namesArray[2]}");
+    }
+}
