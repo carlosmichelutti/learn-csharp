fbe97360865405b79daab83a24ab1dd7c1d577d9
Carlos Michelutti <carlos.stz590@gmail.com>
2025-12-01 22:36:16 -0300

    Create exercise35.

---
+++ exercise35/exercise35/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace exercise35;
+
+public class Program
+{
+    public static void Main()
+    {
+        int totalSales = 0;
+        int salePrice;
+
+        do
+        {
+            Console.Write("Enter the sale amount (or 0 to exit): ");
+            salePrice = Convert.ToInt32(Console.ReadLine());
+            totalSales += salePrice;
+        } while (salePrice != 0);
+
+        Console.WriteLine($"Total sales for the day: ${totalSales}");
+    }
+}
