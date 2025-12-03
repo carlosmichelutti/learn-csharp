0ed07ce376e8c02efbf40f11031c73ea2663a52c
Carlos Michelutti <carlos.stz590@gmail.com>
2025-12-03 19:09:12 -0300

    Create exercise42.

---
+++ exercise42/exercise42/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace exercise42;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Enter a number to calculate the multiplication table: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"{number} x {i} = {number * i}");
+        }
+    }
+}
