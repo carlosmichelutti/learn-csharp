9193f1bc65c99320277c6776dfec48745d1392c5
Carlos Michelutti <carlos.stz590@gmail.com>
2025-12-03 19:02:38 -0300

    Create exercise41.

---
+++ exercise41/exercise41/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace exercise41;
+
+public class Program
+{
+    public static void Main()
+    {
+        for (int number = 1; number <= 20; number++)
+        {
+            if (number % 3 == 0)
+            {
+                continue;
+            }
+            Console.WriteLine(number);
+        }
+    }
+}
