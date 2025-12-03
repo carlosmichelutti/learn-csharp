8031512d3eb5f318fcae5417f65b8ce7cba525d0
Carlos Michelutti <carlos.stz590@gmail.com>
2025-12-03 19:19:49 -0300

    Create exercise44.

---
+++ exercise44/exercise44/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace exercise44;
+
+public class Program
+{
+    public static void Main()
+    {
+        List<double> notes = new List<double> { 8.5, 6.2, 9.1, 5.8, 7.4 };
+
+        foreach (double note in notes)
+        {
+            if (note >= 7.0)
+            {
+                Console.WriteLine($"The student with the grade of {note} is doing very well!");
+            }
+            else
+            {
+                Console.WriteLine($"The student with a grade of {note} is below average!");
+            }
+        }
+    }
+}
