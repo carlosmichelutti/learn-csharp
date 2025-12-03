8b1dd1d2ff2759829b28cd541423b4b0273d37ed
Carlos Michelutti <carlos.stz590@gmail.com>
2025-12-03 19:26:36 -0300

    Create exercise45.

---
+++ exercise45/exercise45/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace exercise45;
+
+public class Program
+{
+    public static void Main()
+    {
+        int quantityOddNumbers = 0;
+        int quantityEvenNumbers = 0;
+
+        for (int number = 1; number <= 10; number++)
+        {
+            Console.Write("Enter a number: ");
+            int numberUser = Convert.ToInt32(Console.ReadLine());
+
+            if (numberUser % 2 == 0)
+            {
+                quantityEvenNumbers++;
+            }
+            else
+            {
+                quantityOddNumbers++;
+            }
+        }
+
+        Console.WriteLine($"You entered {quantityOddNumbers} odd numbers.");
+        Console.WriteLine($"You entered {quantityEvenNumbers} even numbers.");
+    }
+}
