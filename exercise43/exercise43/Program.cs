5e186d928f98395dc96c1b84d1b27c3db0a1cf71
Carlos Michelutti <carlos.stz590@gmail.com>
2025-12-03 19:14:57 -0300

    Create exercise43.

---
+++ exercise43/exercise43/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace exercise43;
+
+public class Program
+{
+    public static void Main()
+    {
+        List<int> notes = new List<int> { 4, 7, 5, 9, 6 };
+
+        foreach (int note in notes)
+        {
+            if (note >= 6)
+            {
+                Console.WriteLine($"Grade {note} - Approved");
+            }
+            else
+            {
+                Console.WriteLine($"Grade {note} - Failed");
+            }
+        }
+    }
+}
