bf80d05a88fc734256bddf5b297d2d0949c28e28
Carlos Michelutti <carlos.stz590@gmail.com>
2025-12-03 22:26:12 -0300

    Create exercise49.

---
+++ exercise49/exercise49/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace exercise49;
+
+public class Program
+{
+    public static void Main()
+    {
+        List<string> mainProductList = new List<string> { "Mouse", "Teclado", "Monitor", "Joystick" };
+        List<string> secondaryProductList = new List<string> { "Headset", "Webcam", "Smartphone", "Mousepad" };
+        List<string> combinedLists = new List<string> (mainProductList.Concat(secondaryProductList));
+
+        foreach (string product in combinedLists)
+        {
+            Console.WriteLine(product);
+        }
+    }
+}
