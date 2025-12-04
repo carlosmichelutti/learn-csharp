a0bdefa97d4f31f5027318156fc93c33ef9c275d
Carlos Michelutti <carlos.stz590@gmail.com>
2025-12-04 19:33:11 -0300

    Create exercise50.

---
+++ exercise50/exercise50/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace exercise50;
+
+public class Program
+{
+    public static void Main()
+    {
+        Dictionary<string, double> studentNotes = new Dictionary<string, double>
+        {
+            { "Carlos", 10.0 },
+            { "João", 7.25 },
+            { "Ana", 5.55 },
+            { "Felipe", 3.10 },
+            { "Bruna", 9.3 }
+        };
+
+        foreach (KeyValuePair<string, double> student in studentNotes)
+        {
+            Console.WriteLine($"Student: {student.Key} - Grade: {student.Value}");
+        }
+    }
+}
