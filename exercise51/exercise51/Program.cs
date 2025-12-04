398bb0023aa634999d6b788ee40d040cc0a76349
Carlos Michelutti <carlos.stz590@gmail.com>
2025-12-04 19:39:58 -0300

    Create exercise51.

---
+++ exercise51/exercise51/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace exercise51;
+
+public class Program
+{
+    public static void Main()
+    {
+        Dictionary<int, string> inventory = new Dictionary<int, string>()
+        {
+            { 1, "Longsword" },
+            { 2, "Short Bow" },
+            { 3, "Iron Shield" }
+        };
+
+        Console.WriteLine("Items in the initial inventory:");
+        foreach (KeyValuePair<int, string> item in inventory)
+        {
+            Console.WriteLine($"ID: {item.Key} - {item.Value}");
+        }
+        Console.WriteLine("");
+
+        inventory.Remove(2);
+        inventory.Add(4, "Life Potion");
+
+        Console.WriteLine("Items in the updated inventory:");
+        foreach (KeyValuePair<int, string> item in inventory)
+        {
+            Console.WriteLine($"ID: {item.Key} - {item.Value}");
+        }
+        Console.WriteLine("");
+    }
+}
