6590e287d7e02aa17bec2cb72bcf84a99ccfc1d5
Carlos Michelutti <carlos.stz590@gmail.com>
2025-12-04 19:44:31 -0300

    Create exercse52.

---
+++ exercise52/exercise52/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace exercise52;
+
+public class Program
+{
+    public static void Main()
+    {
+        Dictionary<string, string> tasks = new Dictionary<string, string>()
+        {
+            { "Refactor login module", "Ana" },
+            { "Testing the payments API", "Pedro" }
+        };
+
+        tasks.Clear();
+
+        tasks.Add("Implement OAuth authentication", "João");
+        tasks.Add("Optimize SQL queries", "Maria");
+        tasks.Add("Update documentation", "Carlos");
+
+        Console.WriteLine("Tasks for the next sprint:");
+        foreach (KeyValuePair<string, string> task in tasks)
+        {
+            Console.WriteLine($"- {task.Key}: {task.Value}");
+        }
+    }
+}
