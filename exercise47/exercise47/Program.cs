41962c4d947a6ae6999e00f6132cfa0eac26f91e
Carlos Michelutti <carlos.stz590@gmail.com>
2025-12-03 22:06:25 -0300

    Create exercise47.

---
+++ exercise47/exercise47/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace exercise47;
+
+public class Program
+{
+    public static void Main()
+    {
+        List<string> message = new List<string> { "o", "d", "n", "u", "m", " ", "á", "l", "o" };
+        message.Reverse();
+
+        foreach(string character in message)
+        {
+            Console.WriteLine(character);
+        }
+    }
+}
