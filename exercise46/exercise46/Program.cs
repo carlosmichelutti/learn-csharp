6ffb3ddb775977800ffc8d4e818ab250e35c88d5
Carlos Michelutti <carlos.stz590@gmail.com>
2025-12-03 22:05:57 -0300

    Create exercise46.

---
+++ exercise46/exercise46/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace exercise46;
+
+public class Program
+{
+    public static void Main()
+    {
+        HashSet<int> scores = new HashSet<int> {};
+        scores.Add(150);
+        scores.Add(90);
+        scores.Add(200);
+        scores.Add(120);
+        scores.Add(150);
+        scores.Add(80);
+        scores.Add(180);
+        scores.Add(200);
+
+        List<int> scoresOrdered = new List<int>(scores);
+        scoresOrdered.Sort();
+
+        Console.WriteLine("Unique scores in ascending order:");
+        foreach (int score in scoresOrdered)
+        {
+            Console.WriteLine(score);
+        }
+    }
+}
