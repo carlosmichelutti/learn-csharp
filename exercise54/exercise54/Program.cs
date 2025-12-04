778b574ffe0be82d321fcb4bff8ee03b53da758c
Carlos Michelutti <carlos.stz590@gmail.com>
2025-12-04 20:43:37 -0300

    Create exercise54.

---
+++ exercise54/exercise54/Program.cs
@@ -0,0 +1,4 @@
+﻿Ticket ticket = new Ticket("Lucas Souza", "Paris");
+
+Console.WriteLine($"Passenger: {ticket.Passenger}");
+Console.WriteLine($"Destination: {ticket.Destination}");
---
+++ exercise54/exercise54/Ticket.cs
@@ -0,0 +1,11 @@
+﻿public class Ticket
+{
+    public string Passenger { get; set; }
+    public string Destination { get; set; }
+
+    public Ticket(string passenger, string destination)
+    {
+        Passenger = passenger;
+        Destination = destination;
+    }
+}
