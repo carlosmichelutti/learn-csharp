7d3e112cbf7177987f277e3c781b36be1d533382
Carlos Michelutti <carlos.stz590@gmail.com>
2025-12-04 21:55:06 -0300

    Create exercise58.

---
+++ exercise58/exercise58/Movie.cs
@@ -0,0 +1,28 @@
+﻿public class Movie
+{
+    public string Title { get; set; }
+    public int AgeClassification { get; set; }
+
+    public Movie(string title, int ageClassification)
+    {
+        Title = title;
+        AgeClassification = ageClassification;
+    }
+
+    public bool CanWatch(int userAge)
+    {
+        return userAge >= AgeClassification;
+    }
+
+    public void DisplayResult(int userAge)
+    {
+        if (CanWatch(userAge))
+        {
+            Console.WriteLine($"Users aged {userAge} and over can watch the movie {Title}.");
+        }
+        else
+        {
+            Console.WriteLine($"Users aged {userAge} and under cannot watch the movie {Title}.");
+        }
+    }
+}
---
+++ exercise58/exercise58/Program.cs
@@ -0,0 +1,3 @@
+﻿Movie movie = new Movie("Matrix", 16);
+movie.DisplayResult(14);
+movie.DisplayResult(20);
