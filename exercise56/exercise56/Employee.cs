e874d362f030fc3c4878e6cc83f9dc5a0b051d36
Carlos Michelutti <carlos.stz590@gmail.com>
2025-12-04 21:29:07 -0300

    Create exercise56.

---
+++ exercise56/exercise56/Employee.cs
@@ -0,0 +1,24 @@
+﻿public class Employee
+{
+    public string Name { get; set; }
+    public string Position { get; set; }
+
+    public Employee(string name, string position)
+    {
+        Name = name;
+        Position = position;
+    }
+
+    public void Promote(string newPosition)
+    {
+        if (newPosition == Position)
+        {
+            Console.WriteLine("Error: The new position must be different from the current position.\n");
+        }
+        else
+        {
+            Position = newPosition;
+            Console.WriteLine("Promotion successfully completed!\n");
+        }
+    }
+}
---
+++ exercise56/exercise56/Program.cs
@@ -0,0 +1,13 @@
+﻿Employee employee = new Employee("Carlos Pereira", "Administrative Assistant");
+
+Console.WriteLine($"Employee: {employee.Name}");
+Console.WriteLine($"Current Position: {employee.Position}");
+Console.WriteLine("");
+
+employee.Promote("Administrative Assistant");
+employee.Promote("Project Analyst");
+
+Console.WriteLine("--- After promotion ---");
+Console.WriteLine($"Employee: {employee.Name}");
+Console.WriteLine($"Current Position: {employee.Position}");
+Console.WriteLine("");
