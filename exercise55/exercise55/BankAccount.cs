a08cdbd443adb3c21e9a24cb8054b26d82a23fa4
Carlos Michelutti <carlos.stz590@gmail.com>
2025-12-04 21:18:57 -0300

    Create exercise55.

---
+++ exercise55/exercise55/BankAccount.cs
@@ -0,0 +1,16 @@
+﻿public class BankAccount
+{
+    public string AccountNumber { get; set; }
+    public double Balance { get; set; }
+
+    public BankAccount(string accountNumber, double balance)
+    {
+        AccountNumber = accountNumber;
+        Balance = balance;
+    }
+
+    public void Deposit(double value)
+    {
+        Balance += value;
+    }
+}
---
+++ exercise55/exercise55/Program.cs
@@ -0,0 +1,5 @@
+﻿BankAccount account = new BankAccount("78901-2", 1000.00);
+account.Deposit(500.00);
+
+Console.WriteLine($"Account: {account.AccountNumber}");
+Console.WriteLine($"Current balance: {account.Balance:F2}");
