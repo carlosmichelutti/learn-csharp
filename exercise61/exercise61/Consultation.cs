083658ff2b644934f16be7acc568eeea34ad1fa8
Carlos Michelutti <carlos.stz590@gmail.com>
2025-12-05 21:20:57 -0300

    Create exercise61.

---
+++ exercise61/exercise61/Consultation.cs
@@ -0,0 +1,33 @@
+﻿public class Consultation
+{
+    public string PatientName { get; set; }
+    public string DoctorName { get; set; }
+    public DateTime ConsultationDate { get; set; }
+    public bool Rescheduled { get; set; }
+
+    public Consultation(string patientName, string doctorName, DateTime consultationDate)
+    {
+        PatientName = patientName;
+        DoctorName = doctorName;
+        ConsultationDate = consultationDate;
+        Rescheduled = false;
+    }
+
+    public void reschedule(DateTime newDate)
+    {
+        ConsultationDate = newDate;
+        Rescheduled = true;
+    }
+
+    public void display_consultation()
+    {
+        if (Rescheduled)
+        {
+            Console.WriteLine($"Appointment scheduled with {DoctorName} for patient {PatientName}.");
+            Console.WriteLine($"New date: {ConsultationDate.ToString("dd/MM/yyyy")}");
+            return;
+        }
+        Console.WriteLine($"Appointment scheduled with {DoctorName} for patient {PatientName}.");
+        Console.WriteLine($"Date: {ConsultationDate.ToString("dd/MM/yyyy")}");
+    }
+}
---
+++ exercise61/exercise61/Program.cs
@@ -0,0 +1,4 @@
+﻿Consultation consultation = new Consultation("João Lima", "Dra. Renata", new DateTime(2025, 5, 20));
+consultation.display_consultation();
+consultation.reschedule(new DateTime(2025, 5, 25));
+consultation.display_consultation();
