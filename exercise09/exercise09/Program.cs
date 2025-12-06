4f9152b0e15674a31b661479d06a4f89a87ae060
Carlos Michelutti <carlos.stz590@gmail.com>
2025-12-06 14:50:00 -0300

    Update exercise09.

--- exercise09/exercise09/Program.cs
+++ exercise09/exercise09/Program.cs
@@ -5,8 +5,8 @@ public class Program
     public static void Main()
     {
         decimal currentSalary = 3000.00m;
-        decimal percentageIncrease = 20.00m;
-        decimal newSalary = currentSalary * (1 + (percentageIncrease / 100));
+        decimal percentAgeIncrease = 20.00m;
+        decimal newSalary = currentSalary * (1 + (percentAgeIncrease / 100));
 
         Console.WriteLine($"The new salary is ${newSalary:F2}");
     }
