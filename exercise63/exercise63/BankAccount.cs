0426ef99fd9b6930a587d4cfc668b5080703cb99
Carlos Michelutti <carlos.stz590@gmail.com>
2025-12-06 22:22:40 -0300

    Create exercise63.

---
+++ exercise63/exercise63/BankAccount.cs
@@ -0,0 +1,23 @@
+﻿public class BankAccount
+{
+    public int AccountNumber { get; set; }
+    public string Holder { get; set; }
+    public double Balance { get; set; }
+    public string Password { get; set; }
+
+    public BankAccount(int accountNumber, string holder, double balance, string password)
+    {
+        AccountNumber = accountNumber;
+        Holder = holder;
+        Balance = balance;
+        Password = new string('*', 3) + password[3..^3] + new string('*', 3);
+    }
+
+    public void ShowDetails()
+    {
+        Console.WriteLine($"Account Number: {AccountNumber}");
+        Console.WriteLine($"Holder: {Holder}");
+        Console.WriteLine($"Balance: {Balance:F2}");
+        Console.WriteLine($"Password: {Password}");
+    }
+}
---
+++ exercise63/exercise63/Program.cs
@@ -0,0 +1,2 @@
+﻿BankAccount bankAccount = new BankAccount(1, "Carlos", 300.00, "123456789");
+bankAccount.ShowDetails();
