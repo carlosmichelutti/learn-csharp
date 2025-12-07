eb354b2d802ebd9b1f5ba57913aaf383ab553784
Carlos Michelutti <carlos.stz590@gmail.com>
2025-12-06 22:22:59 -0300

    Create exercise64.

---
+++ exercise64/exercise64/Car.cs
@@ -0,0 +1,58 @@
+﻿public class Car
+{
+    private int _year;
+    public string Manufacturer { get; set; }
+    public string Model { get; set; }
+    public int Year
+    {
+        get => _year;
+        set
+        {
+            if (value < 1960 || value > 2023)
+                throw new ArgumentOutOfRangeException(nameof(Year));
+            _year = value;
+        }
+    }
+    public int Speed { get; set; } = 0;
+    public string DetailedDescription => $"Car model: {this.Manufacturer} {this.Model} {this._year}";
+
+    public Car(string manufacturer, string model, int year)
+    {
+        Manufacturer = manufacturer;
+        Model = model;
+        Year = year;
+    }
+
+    public void Accelerate()
+    {
+        Console.WriteLine("Accelerating...");
+        do
+        {
+            Speed += 5;
+            Console.WriteLine($"Speed {Speed} km/h...");
+
+            if (Speed >= 100)
+                break;
+
+        } while (true);
+    }
+
+    public void Brake()
+    {
+        Console.WriteLine("Braking...");
+        do
+        {
+            Speed -= 5;
+            Console.WriteLine($"Speed {Speed} km/h...");
+
+            if (Speed <= 0)
+                break;
+
+        } while (true);
+    }
+
+    public static void Honk()
+    {
+        Console.WriteLine("Beep Beep");
+    }
+}
---
+++ exercise64/exercise64/Program.cs
@@ -0,0 +1,5 @@
+﻿Car car = new Car("Ferrari", "Ferrari 812", 2023);
+Console.WriteLine(car.DetailedDescription);
+car.Accelerate();
+car.Brake();
+Car.Honk();
