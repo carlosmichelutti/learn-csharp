5abbf798c4568610ba964e4c69c405aed6b609b9
Carlos Michelutti <carlos.stz590@gmail.com>
2025-12-06 22:23:05 -0300

    Create exercise65.

---
+++ exercise65/exercise65/Product.cs
@@ -0,0 +1,36 @@
+﻿public class Product
+{
+    private double _price;
+    private int _stock;
+    public string Name { get; set; }
+    public string Brand { get; set; }
+    public double Price
+    {
+        get => _price;
+        set
+        {
+            if (value < 0)
+                throw new ArgumentOutOfRangeException(nameof(Price), "Price cannot be negative.");
+            _price = value;
+        }
+    }
+    public int Stock
+    {
+        get => _stock;
+        set
+        {
+            if (value < 0)
+                throw new ArgumentOutOfRangeException(nameof(Stock), "Stock cannot be negative.");
+            _stock = value;
+        }
+    }
+    public string DetailedDescription => $"Product: {this.Name} - Brand: {this.Brand} - Stock: {this.Stock} - Price: {this.Price:F2}";
+
+    public Product(string name, string brand, double price, int stock)
+    {
+        Name = name;
+        Brand = brand;
+        Price = price;
+        Stock = stock;
+    }
+}
---
+++ exercise65/exercise65/Program.cs
@@ -0,0 +1,2 @@
+﻿Product product = new Product("Candy", "Halls", 4.99, 40);
+Console.WriteLine(product.DetailedDescription);
