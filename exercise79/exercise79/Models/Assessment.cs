cd1533973acadf838708aaf786d8b861e32dd922
Carlos Michelutti <carlos.stz590@gmail.com>
2025-12-14 17:27:20 -0300

    Create exercise79;

---
+++ exercise79/exercise79/Models/Assessment.cs
@@ -0,0 +1,23 @@
+﻿namespace exercise79.Models;
+
+internal class Assessment
+{
+    public string Student { get; set; }
+    public double Grade { get; private set; }
+
+    public Assessment(string student)
+    {
+        Student = student;
+    }
+
+    public void AssignGrade(double newGrade)
+    {
+        if (newGrade < 0 || newGrade > 10) Console.WriteLine("Error: The grade must be between 0 and 10.");
+        Grade = newGrade;
+    }
+
+    public override string ToString()
+    {
+        return $"Student: {Student} - Grade awarded: {Grade:F2}";
+    }
+}
---
+++ exercise79/exercise79/Program.cs
@@ -0,0 +1,6 @@
+﻿using exercise79.Models;
+
+Assessment assessment = new Assessment("Carlos Eduardo");
+assessment.AssignGrade(11);
+assessment.AssignGrade(9);
+Console.WriteLine(assessment);
