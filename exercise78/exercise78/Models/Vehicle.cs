fb209ee7a05086dfc747ce5cbeed62bc3ae2e25e
Carlos Michelutti <carlos.stz590@gmail.com>
2025-12-14 17:13:39 -0300

    Create exercise78.

---
+++ exercise78/exercise78/Models/Vehicle.cs
@@ -0,0 +1,24 @@
+﻿namespace exercise78.Models;
+
+internal class Vehicle
+{
+    public string Plate { get; set; }
+    public double CurrentSpeed { get; private set; }
+
+    public Vehicle(string plate)
+    {
+        Plate = plate;
+        CurrentSpeed = 0;
+    }
+
+    public void UpdateSpeed(double newSpeed)
+    {
+        CurrentSpeed = newSpeed;
+    }
+
+    public override string ToString()
+    {
+        return $"Vehicle: {Plate} - Current speed: {CurrentSpeed:F2} km/h";
+    }
+}
+
---
+++ exercise78/exercise78/Program.cs
@@ -0,0 +1,9 @@
+﻿using exercise78.Models;
+
+Vehicle vehicle = new Vehicle("ABC-1234");
+
+vehicle.UpdateSpeed(72);
+Console.WriteLine(vehicle);
+
+vehicle.UpdateSpeed(103.5);
+Console.WriteLine(vehicle);
