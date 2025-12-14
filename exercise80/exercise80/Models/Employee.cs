fe5ecb911cb77697d2836ec163bd4979d1676ba4
Carlos Michelutti <carlos.stz590@gmail.com>
2025-12-14 20:55:50 -0300

    Create exercise80.

---
+++ exercise80/exercise80/Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿namespace exercise80.Models;
+
+internal class Employee
+{
+    public string Name { get; }
+    public double Salary { get; private set; }
+
+    public Employee(string name, double salary)
+    {
+        Name = name;
+        Salary = salary;
+    }
+
+    public void AdjustSalary(double newSalary)
+    {
+        if (newSalary < Salary)
+        {
+            Console.WriteLine("Error: The new salary must be higher than the current one.");
+        }
+        else
+        {
+            Salary = newSalary;
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"Employee: {Name} - Current salary: ${Salary:F2}";
+    }
+}
---
+++ exercise80/exercise80/Program.cs
@@ -0,0 +1,9 @@
+﻿using exercise80.Models;
+
+Employee employee = new Employee("Carlos Eduardo", 2000);
+Console.WriteLine(employee);
+
+employee.AdjustSalary(1500);
+employee.AdjustSalary(5000);
+
+Console.WriteLine(employee);
