445b4957d9d4e31ef21e3a3dd9f1b9e14c756cd7
Carlos Michelutti <carlos.stz590@gmail.com>
2025-12-14 21:36:36 -0300

    Create exercise81.

---
+++ exercise81/exercise81/Models/Project.cs
@@ -0,0 +1,30 @@
+﻿namespace exercise81.Models;
+
+internal class Project
+{
+    private List<string> _tasks;
+    public string Name { get; set; }
+    public int NumberTasks => _tasks.Count;
+
+    public Project(string name)
+    {
+        Name = name;
+        _tasks = new List<string>();
+    }
+
+    public void AddTask(string task)
+    {
+        _tasks.Add(task);
+    }
+
+    public void DisplayTasks()
+    {
+        Console.WriteLine($"Project: {Name}");
+        Console.WriteLine("Tasks:");
+        foreach (string task in _tasks)
+        {
+            Console.WriteLine($" - {task}");
+        }
+        Console.WriteLine($"Total: {NumberTasks} tasks");
+    }
+}
---
+++ exercise81/exercise81/Program.cs
@@ -0,0 +1,7 @@
+﻿using exercise81.Models;
+
+Project project = new Project("Sistema de Inventário");
+project.AddTask("Create login screen");
+project.AddTask("Implement database");
+
+project.DisplayTasks();
