3092ddec7a993d1e0c45df9828c97740528ad844
Carlos Michelutti <carlos.stz590@gmail.com>
2025-12-15 21:59:32 -0300

    Create exercise84.

---
+++ exercise84/exercise84/Models/VipCustomer.cs
@@ -0,0 +1,19 @@
+﻿namespace exercise84.Models;
+
+internal class VipCustomer : Person
+{
+    public string Level { get; }
+    public string VipCode { get; }
+
+    public VipCustomer(string name, int age, string level, string vipCode)
+        : base(name, age)
+    {
+        Level = level;
+        VipCode = vipCode;
+    }
+
+    public override string ToString()
+    {
+        return $"Welcome, VIP customer: {Name}\nAge: {Age}\nLevel: {Level}\nVIP Code: {VipCode}\n";
+    }
+}
---
+++ exercise84/exercise84/Program.cs
@@ -0,0 +1,7 @@
+﻿using exercise84.Models;
+
+VipCustomer cliente1 = new VipCustomer("Renata", 32, "Gold", "VIP123A");
+VipCustomer cliente2 = new VipCustomer("Leonardo", 40, "Diamond", "VIP789X");
+
+Console.WriteLine(cliente1);
+Console.WriteLine(cliente2);
