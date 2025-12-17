9cc9f526dd49685ece842a169f2ea432f9fc5b23
Carlos Michelutti <carlos.stz590@gmail.com>
2025-12-17 20:03:18 -0300

    Create exercise88.

---
+++ exercise88/exercise88/Models/Calculator.cs
@@ -0,0 +1,19 @@
+﻿namespace exercise88.Models;
+
+internal class Calculator
+{
+    public int Sum(int number1, int number2)
+    {
+        return number1 + number2;
+    }
+
+    public int Sum(int number1, int number2, int number3)
+    {
+        return number1 + number2 + number3;
+    }
+
+    public double Sum(double number1, double number2)
+    {
+        return number1 + number2;
+    }
+}
---
+++ exercise88/exercise88/Program.cs
@@ -0,0 +1,7 @@
+﻿using exercise88.Models;
+
+Calculator calculator = new Calculator();
+
+Console.WriteLine(calculator.Sum(10, 20));
+Console.WriteLine(calculator.Sum(5, 10, 15));
+Console.WriteLine(calculator.Sum(3.5, 2.8));
