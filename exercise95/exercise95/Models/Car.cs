35a1c780bfda6afffadb25ea227f42a2f4b3a044
Carlos Michelutti <carlos.stz590@gmail.com>
2025-12-19 22:30:31 -0300

    Create exercise95.

---
+++ exercise95/exercise95/Models/Car.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace exercise94.Models;
+
+internal class Car
+{
+    [JsonPropertyName("marca")]
+    public string? Brand { get; set; }
+
+    [JsonPropertyName("modelo")]
+    public string? Model { get; set; }
+
+    [JsonPropertyName("ano")]
+    public int? Year { get; set; }
+
+    [JsonPropertyName("tipo")]
+    public string? Type { get; set; }
+
+    public string DetailedDescription => $"Brand: {Brand} - Model: {Model} - Year: {Year} - Type: {Type}";
+}
