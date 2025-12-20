621b8acb0ffea4a28b52335b4a376c87afe0a1d4
Carlos Michelutti <carlos.stz590@gmail.com>
2025-12-19 22:43:17 -0300

    Create exercise96.

---
+++ exercise96/exercise96/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace exercise96.Models;
+
+internal class Book
+{
+    [JsonPropertyName("titulo")]
+    public string? Title { get; set; }
+
+    [JsonPropertyName("autor")]
+    public string? Author { get; set; }
+
+    [JsonPropertyName("ano_publicacao")]
+    public int? YearPublication { get; set; }
+
+    [JsonPropertyName("genero")]
+    public string? Gender { get; set; }
+
+    [JsonPropertyName("paginas")]
+    public int? Pages { get; set; }
+
+    public string DetailedDescription => $"Title: {Title} - Author: {Author} - Year of Publication {YearPublication} - Gender: {Gender} - Quantity Pages {Pages}";
+}
---
+++ exercise96/exercise96/Program.cs
@@ -0,0 +1,19 @@
+﻿using exercise96.Models;
+using System.Text.Json;
+
+using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        string response = await client.GetStringAsync("https://raw.githubusercontent.com/ArthurOcFernandes/Exerc-cios-C-/curso-4-aula-2/Jsons/Livros.json");
+        List<Book> books = JsonSerializer.Deserialize<List<Book>>(response)!;
+        foreach (Book book in books)
+        {
+            Console.WriteLine(book.DetailedDescription);
+        }
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine($"Error: {e.Message}");
+    }
+}
