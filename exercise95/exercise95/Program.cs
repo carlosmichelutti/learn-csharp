977a53d2e37c23537fc5826d92c2d50266c89ae5
Carlos Michelutti <carlos.stz590@gmail.com>
2025-12-19 22:42:51 -0300

    Update exercise95.

--- exercise95/exercise95/Program.cs
+++ exercise95/exercise95/Program.cs
@@ -6,8 +6,8 @@ using (HttpClient client = new HttpClient())
     try
     {
         string response = await client.GetStringAsync("https://raw.githubusercontent.com/ArthurOcFernandes/Exerc-cios-C-/curso-4-aula-2/Jsons/Carros.json");
-        List<Car> countries = JsonSerializer.Deserialize<List<Car>>(response)!;
-        foreach (Car car in countries)
+        List<Car> cars = JsonSerializer.Deserialize<List<Car>>(response)!;
+        foreach (Car car in cars)
         {
             Console.WriteLine(car.DetailedDescription);
         }
