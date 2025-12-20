3247a34f18411d2dfd967022c829281fb5044995
Carlos Michelutti <carlos.stz590@gmail.com>
2025-12-19 22:22:56 -0300

    Create exercise94.

---
+++ exercise94/exercise94/Models/Country.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace exercise94.Models;
+
+internal class Country
+{
+    [JsonPropertyName("nome")]
+    public string? Name { get; set; }
+
+    [JsonPropertyName("capital")]
+    public string? Capital { get; set; }
+
+    [JsonPropertyName("populacao")]
+    public int? Population { get; set; }
+
+    [JsonPropertyName("continente")]
+    public string? Continent { get; set; }
+
+    [JsonPropertyName("idioma")]
+    public string? Language { get; set; }
+
+    public string DetailedDescription => $"Name: {Name} - Capital: {Capital} - Population: {Population} - Language: {Language}";
+}
---
+++ exercise94/exercise94/Program.cs
@@ -0,0 +1,19 @@
+﻿using exercise94.Models;
+using System.Text.Json;
+
+using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        string response = await client.GetStringAsync("https://raw.githubusercontent.com/ArthurOcFernandes/Exerc-cios-C-/curso-4-aula-2/Jsons/Paises.json");
+        List<Country> countries = JsonSerializer.Deserialize<List<Country>>(response)!;
+        foreach(Country country in countries)
+        {
+            Console.WriteLine(country.DetailedDescription);
+        }
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine($"Error: {e.Message}");
+    }
+}
